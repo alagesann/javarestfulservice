a19da1165e9111be1c02ca6d20c1b199a028dc7b
Alaggesan Palani <alagesann@gmail.com>
2016-06-19 17:00:48 +0530

    Employee Initial change

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+package com.javarestful;
+/**
+ * Created by apalani on 6/19/16.
+ */
+public class Employee {
+    String Name;
+    int EmpId;
+    public Employee(String name, int empId){
+        this.Name=name;
+        this.EmpId=empId;
+    }
+    public void display()
+    {
+        System.out.println("Name: "+this.Name);
+        System.out.println("EmpId: "+this.EmpId);
+    }
+}
--- src/com/javarestful/Main.java
+++ src/com/javarestful/Main.java
@@ -1,8 +1,12 @@
 package com.javarestful;
 
+import com.javarestful.Employee;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Javarestful");
+        Employee emp=new Employee("Arjun",12345);
+        emp.display();
     }
 }
